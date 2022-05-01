1d6d10bc32d6f5c4c8a2172a87494c2c86b38b80
Saad Alsaad <1185480@student.birzeit.edu>
2022-05-01 19:20:00 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -20,7 +20,8 @@ class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("Saad", "saadalsaad00@gmail.com")]
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
